fc33b6a093ecb12e68ab9681499b41292877a91c
harshedabdulla <harshed2004@gmail.com>
2023-11-05 01:10:07 +0530

    story mode

--- main.js
+++ main.js
@@ -1,14 +1,78 @@
-// Mood Over the Past 6 Hours Chart
+import {initializeApp } from 'https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js'
+import { getDatabase, set, get, ref, child, push, onValue } from 'https://www.gstatic.com/firebasejs/10.3.0/firebase-database.js'
+  
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBLOrV8K_-MYavFlHf7HiYwUHmUQSt2YzA",
+    databaseURL: " https://empath-866ce-default-rtdb.asia-southeast1.firebasedatabase.app",
+    authDomain: "empath-866ce.firebaseapp.com",
+    projectId: "empath-866ce",
+    storageBucket: "empath-866ce.appspot.com",
+    messagingSenderId: "688144290902",
+    appId: "1:688144290902:web:2cb7523e3db636e6e717f7",
+    measurementId: "G-BC0BZWP1VD"
+};
+
+const app = initializeApp(firebaseConfig);
+const db  = getDatabase()
+const dbRef = ref(getDatabase());
+
+function sendtopopup(){
+    
+
+    get(child(dbRef, `data`)).then((snapshot) => {
+		if (snapshot.exists()) {
+            let data = snapshot.val()
+            const latest = 6
+            const objectKeys = Object.keys(data);
+            data = objectKeys.slice(-latest).reduce((result, key) => {
+            result[key] = data[key];
+            return result;
+            }, {});
+
+            console.log(data);
+
+            const allCategories = [];
+
+            for (const key in data) {
+              const category = data[key].mood;
+              allCategories.push(category);
+            }
+            const categoryCounts = allCategories.reduce((acc, category) => {
+                acc[category] = (acc[category] || 0) + 1;
+                return acc;
+              }, {});
+              
+              // Step 2: Convert the object into an array of objects for sorting
+              const categoryArray = Object.entries(categoryCounts).map(([category, count]) => ({
+                category,
+                count,
+              }));
+              
+              // Step 3: Sort the array based on the frequency in descending order
+              categoryArray.sort((a, b) => b.count - a.count);
+              
+              // Step 4: Get the top 3 categories
+              
+            const top3Categories = categoryArray.slice(0, 3);
+            console.log(top3Categories[0]);
+            // Mood Over the Past 6 Hours Chart
 const moodChartCanvas = document.getElementById("moodChart");
 
 // Sample data for the three emotions in the "Current Mood" chart
+const x = top3Categories[0] ? top3Categories[0].category : "";
+const y = top3Categories[1] ? top3Categories[1].category : "";
+const z = top3Categories[2] ? top3Categories[2].category : "";
+const a = top3Categories[0] ? top3Categories[0].count : "";
+const b = top3Categories[1] ? top3Categories[1].count : "";
+const c = top3Categories[2] ? top3Categories[2].count : "";
+
 const currentMoodData = {
-    labels: ["😐", "🧐", "😞"],
-    data: [30, 40, 30] // Adjust the values as needed
+    labels: [x, y, z],
+    data: [a,b,c] // Adjust the values as needed
 };
 
 const currentMoodChartCanvas = document.getElementById("currentMoodChart");
-
 new Chart(currentMoodChartCanvas.getContext("2d"), {
     type: 'pie',
     data: {
@@ -29,41 +93,37 @@ new Chart(currentMoodChartCanvas.getContext("2d"), {
             enabled: false // Disable tooltips
         }
     }
-});
-
-
-
-
+}); 
 // Sample data for the top 3 emotions at each hour
-const data = {
+const data1 = {
     labels: ["6 hours ago", "5 hours ago", "4 hours ago", "3 hours ago", "2 hours ago", "1 hour ago"],
     emotions: {
-        emotion1: [70, 80, 85, 90, 75, 80], // Emotion 1 data
-        emotion2: [60, 90, 75, 80, 70, 75], // Emotion 2 data
-        emotion3: [50, 83, 65, 93, 60, 65], // Emotion 3 data
+        emotion1: [70, 80, 85, 90, 75, 80], // Emotion 1 data1
+        emotion2: [60, 90, 75, 80, 70, 75], // Emotion 2 data1
+        emotion3: [50, 83, 65, 93, 60, 65], // Emotion 3 data1
     }
 };
 
 const chartData = {
-    labels: data.labels,
+    labels: data1.labels,
     datasets: [
         {
             label: 'Emotion 1',
-            data: data.emotions.emotion1,
+            data: data1.emotions.emotion1,
             backgroundColor: 'rgba(75, 192, 192, 0.2)',
             borderColor: 'rgba(75, 192, 192, 1)',
             borderWidth: 1
         },
         {
             label: 'Emotion 2',
-            data: data.emotions.emotion2,
+            data: data1.emotions.emotion2,
             backgroundColor: 'rgba(255, 99, 132, 0.2)',
             borderColor: 'rgba(255, 99, 132, 1)',
             borderWidth: 1
         },
         {
             label: 'Emotion 3',
-            data: data.emotions.emotion3,
+            data: data1.emotions.emotion3,
             backgroundColor: 'rgba(255, 205, 86, 0.2)',
             borderColor: 'rgba(255, 205, 86, 1)',
             borderWidth: 1
@@ -92,6 +152,21 @@ new Chart(moodChartCanvas.getContext("2d"), {
     data: chartData,
     options: chartOptions
 });
+
+
+
+
+
+}
+})
+}
+sendtopopup();
+
+
+
+
+
+
 
 
 // Daily Mood Trends Chart
